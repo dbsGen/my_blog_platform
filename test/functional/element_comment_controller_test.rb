1d9371f9f407efe51c2c69230eb45ee53c9c9ab4
Gen <gen@gen-Aspire-V3-551G.(none)>
2013-06-17 22:01:59 +0800

    Some file forgot

---
+++ test/functional/element_comment_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ElementCommentControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/element_comment_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ElementCommentHelperTest < ActionView::TestCase
+end
